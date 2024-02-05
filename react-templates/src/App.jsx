39ff672c49d6fe3c563f46bd73b63c1072d16cf0
Asad Tariq <asadtariq.at66@gmail.com>
2024-02-05 23:24:16 +0500

    tailwind setup

---
+++ react-templates/src/App.jsx
@@ -0,0 +1,12 @@
+import Header1 from "./Components/Headers/HeaderWithMenu/header1"
+
+function App() {
+
+  return (
+    <>
+      <Header1 />
+    </>
+  )
+}
+
+export default App
---
+++ react-templates/src/Components/Headers/HeaderWithMenu/header1.jsx
@@ -0,0 +1,16 @@
+/**
+ * Header component with menu for mobile view
+ *  */
+
+import "./header1.css"
+
+const Header1 = () => {
+    return (
+        <div>
+
+            <h1 className="text-5xl font-bold">Hello World</h1>
+        </div>
+    )
+}
+
+export default Header1;
