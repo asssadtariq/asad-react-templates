a3475b42bb5c84e4be88721eb506ee9f6cb14803
Asad Tariq <asadtariq.at66@gmail.com>
2024-02-06 16:06:27 +0500

    header updated - menu added, responsiveness updated

--- react-templates/src/Components/Headers/HeaderWithMenu/header1.jsx
+++ react-templates/src/Components/Headers/HeaderWithMenu/header1.jsx
@@ -2,29 +2,60 @@
  * Header component with menu for mobile view
  *  */
 
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header1 = () => {
+    const [menu, setMenu] = useState(false);
+
+    const handleMenu = () => {
+        setMenu(prevValue => !prevValue);
+    }
+
     return (
-        <div className="bg-slate-100 h-48 grid grid-cols-3 px-16">
-            <div className="flex items-center gap-12">
-                <img className="w-32" src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?w=740&t=st=1707158121~exp=1707158721~hmac=517fb0247effc65b5839863186ec49dda88e4d998d16511537ea201994b6928d" alt="" id="logo-img" />
-                <h1 className="text-black font-semibold text-2xl">LOREM IPSUM</h1>
+        <div className="bg-white px-16 shadow grid">
+            <div className="lg:h-48 md:h-48 h-72 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 py-2">
+                <div className="flex md:flex-row lg:flex-row flex-col items-center gap-12">
+                    <img className="w-32" 
+                        src="https://i.pinimg.com/564x/9f/93/ae/9f93ae8f39417cd575e735bf5f1b1505.jpg" 
+                        alt="" id="logo-img" 
+                    />
+                    <h1 className="text-black font-semibold text-2xl">LOREM IPSUM</h1>
+                </div>
+                <ul className="col-span-2 hidden lg:flex w-full gap-4 items-center justify-end">
+                    <li className="hover:font-semibold cursor-pointer">Home</li>
+                    <li className="hover:font-semibold cursor-pointer">About Us</li>
+                    <li className="hover:font-semibold cursor-pointer">Pricing</li>
+                    <li className="hover:font-semibold cursor-pointer">Our Team</li>
+                    <li className="hover:font-semibold cursor-pointer">Contact Us</li>
+                    <li className="">
+                        <a href="" className="p-3 rounded bg-blue-500 text-white font-semibold hover:bg-blue-600">Join Us</a>
+                    </li>
+                </ul>
+                <div className="lg:col-span-2 flex justify-center md:justify-end lg:justify-end items-center lg:hidden ">
+                    <button 
+                        className="hover:bg-slate-100 p-2"
+                        onClick={handleMenu}
+                    >
+                        <GiHamburgerMenu size={32} />
+                    </button>
+                </div>
+
             </div>
-            <ul className="col-span-2 hidden lg:flex w-full gap-4 items-center justify-end">
-                <li className="hover:font-semibold cursor-pointer">Home</li>
-                <li className="hover:font-semibold cursor-pointer">About Us</li>
-                <li className="hover:font-semibold cursor-pointer">Pricing</li>
-                <li className="hover:font-semibold cursor-pointer">Our Team</li>
-                <li className="hover:font-semibold cursor-pointer">Contact Us</li>
-                <li className="">
-                    <a href="" className="p-3 rounded bg-blue-500 text-white font-semibold hover:bg-blue-600">Join Us</a>
-                </li>
-            </ul>
-            <button className="col-span-2 flex justify-end items-center lg:hidden">
-                <GiHamburgerMenu size={32} />
-            </button>
-            
+            {
+                menu ?             
+                <ul className="lg:hidden grid grid-cols-1 gap-4 py-8">
+                    <li className="hover:font-semibold cursor-pointer">Home</li>
+                    <li className="hover:font-semibold cursor-pointer">About Us</li>
+                    <li className="hover:font-semibold cursor-pointer">Pricing</li>
+                    <li className="hover:font-semibold cursor-pointer">Our Team</li>
+                    <li className="hover:font-semibold cursor-pointer">Contact Us</li>
+                    <li className="">
+                        <a href="" className="p-3 rounded bg-blue-500 text-white font-semibold hover:bg-blue-600">Join Us</a>
+                    </li>
+                </ul>
+                : null    
+            }
         </div>
     )
 }
