3a0dfd2bea6a1508266441fdb416eb1293c3e0f7
Asad Tariq <asadtariq.at66@gmail.com>
2024-05-28 19:54:36 +0500

    MainPage component added

---
+++ react-templates/src/Pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import Header1 from "../Components/Headers/HeaderWithMenu/header1";
+import LogoFooter from "../Components/Footers/FooterWithLogo/FooterWithLogo";
+
+const MainPage = () => {
+    return (
+        <div>
+            <Header1 />
+            <LogoFooter />
+        </div>
+    )
+}
+
+export default MainPage;
